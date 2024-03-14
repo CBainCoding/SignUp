e85fa5f43a520c47eff6cb206f798bc224e1c079
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-14 14:51:32 +0000

    Created signup form and tested account creation works

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
@@ -16,3 +16,27 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2", "experience")
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        "placeholder": "Your username",
+        "class": "form-control mb-3"
+    }))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={
+        "placeholder": "Your email",
+        "class": "form-control mb-3"
+    }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        "placeholder": "Enter password",
+        "class": "form-control mb-3"
+    }))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        "placeholder": "Confirm password",
+        "class": "form-control mb-3"
+    }))
+    experience_choices = (
+        ('beginner', 'Beginner'),
+        ('experienced', 'Experienced'),
+    )
+    experience = forms.ChoiceField(choices=experience_choices, widget=forms.Select(attrs={
+        "class": "form-select mb-3"
+    }))
--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .forms import SignupForm
 
@@ -7,8 +7,17 @@ from .forms import SignupForm
 def index(request):
     return render(request, 'core/index.html')
 
-def signup (request):
-    form = SignupForm()
+def signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect("/login/")
+    else:
+        form = SignupForm()
+    
 
     return render(request, "core/signup.html", {
         "form": form
