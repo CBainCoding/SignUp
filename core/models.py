7415a47d64b36a902dbfd6e447eeae75b35f0bd3
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-15 16:15:40 +0000

    Undid everything from previous few commits, removed new user profile model and admin code until I can properly resolve the issue

--- core/models.py
+++ core/models.py
@@ -1,18 +1,7 @@
 from django.db import models
-from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 # Create your models here.
 
 class Core(models.Model):
     title = models.CharField(max_length=100)
     image = CloudinaryField('image', default='placeholder')
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    experience = models.CharField(max_length=20, choices=(
-        ('beginner', 'Beginner'),
-        ('experienced', 'Experienced'),
-    ))
-
-    def __str__(self):
-        return self.user.username
