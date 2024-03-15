f7e1416fae031d3bcaae1d2ccdf4aaed72a9cf05
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-15 15:06:27 +0000

    Fixed user model not pushing names through to admin panel

--- core/forms.py
+++ core/forms.py
@@ -3,19 +3,14 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
-
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
-    
+
     experience_choices = (
         ('beginner', 'Beginner'),
         ('experienced', 'Experienced'),
     )
     experience = forms.ChoiceField(choices=experience_choices)
-
-    class Meta:
-        model = User
-        fields = ("username", "email", "password1", "password2", "experience")
 
     username = forms.CharField(widget=forms.TextInput(attrs={
         "placeholder": "Your username",
@@ -33,13 +28,13 @@ class SignupForm(UserCreationForm):
         "placeholder": "Confirm password",
         "class": "form-control mb-3"
     }))
-    experience_choices = (
-        ('beginner', 'Beginner'),
-        ('experienced', 'Experienced'),
-    )
     experience = forms.ChoiceField(choices=experience_choices, widget=forms.Select(attrs={
         "class": "form-select mb-3"
     }))
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2", "first_name", "last_name", "experience")
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
