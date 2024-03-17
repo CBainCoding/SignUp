45276fc1681d2851bfc446be2c6e135db2af582f
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-17 10:22:53 +0000

    Wired up UserProfile with signup form

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
+from dashboard.models import UserProfile
 
 class SignupForm(UserCreationForm):
     # Custom fields for experience
