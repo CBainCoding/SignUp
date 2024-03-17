92f62868392a9ce061aca2b9e25d1871a395e080
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-17 09:58:10 +0000

    Created UserProfile model in dashboard

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    experience = models.CharField(max_length=20, choices=(
+        ('beginner', 'Beginner'),
+        ('experienced', 'Experienced'),
+    ))
+
+    def __str__(self):
+        return self.user.username
