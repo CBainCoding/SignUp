c5359c0ef76841da86244e85f83fc975602313dd
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-17 10:26:02 +0000

    Added User Profile view to dashboard

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import UserProfile
 
 # Register your models here.
+
+# Display Experience field in admin panel
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'experience')  # Display these fields in the admin list view
+    search_fields = ['user__username', 'experience']  # Allow searching by username and experience
+
+# Register the model and the admin class
+admin.site.register(UserProfile, UserProfileAdmin)
