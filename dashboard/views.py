81b1ee4bcc5121187ed5fd1a409d8dd3e654488b
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-18 15:17:01 +0000

    Added account page view to dashboard app

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import UserProfile
 
 # Create your views here.
+
+@login_required
+def account_page(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+    context = {
+        'user': request.user,
+        'user_profile': user_profile,
+    }
+    return render(request, 'account_page.html', context)
