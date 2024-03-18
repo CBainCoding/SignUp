4d9d5b260733fa4fa8d149362a6ee4a11933e406
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-18 17:05:05 +0000

    Created edit user view in dashboard

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import UserProfile
 
+#This edits the standard "user" and should pull through to "user profile"
 class UserEditForm(forms.ModelForm):
     first_name = forms.CharField(max_length=30, required=True)
     last_name = forms.CharField(max_length=30, required=True)
@@ -11,6 +12,7 @@ class UserEditForm(forms.ModelForm):
         model = User
         fields = ('first_name', 'last_name', 'email')
 
+# This edits the custom extended user profile
 class UserProfileEditForm(forms.ModelForm):
     class Meta:
         model = UserProfile
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import UserProfile
 
 # Create your views here.
@@ -12,3 +12,19 @@ def account_page(request):
         'user_profile': user_profile,
     }
     return render(request, 'account_page.html', context)
+
+@login_required
+def edit_user(request):
+    if request.method == 'POST':
+        user_form = UserEditForm(instance=request.user, data=request.POST)
+        profile_form = UserProfileEditForm(instance=request.user.userprofile, data=request.POST)
+        
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            return redirect('account_page')  # Redirect to the account page or wherever appropriate
+    else:
+        user_form = UserEditForm(instance=request.user)
+        profile_form = UserProfileEditForm(instance=request.user.userprofile)
+    
+    return render(request, 'edit_account.html', {'user_form': user_form, 'profile_form': profile_form})
