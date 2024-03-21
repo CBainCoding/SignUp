c75411c0a8943feb675f7b45139e30329c3e580c
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-21 16:52:08 +0000

    Removed redundant code to display messages on individual pages as it has been added to base template

--- faqs/views.py
+++ faqs/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
@@ -26,13 +27,28 @@ class FAQCreateView(LoginRequiredMixin, StaffRequiredMixin, CreateView):
     template_name = 'faqs/faq_form.html'
     success_url = reverse_lazy('faqs:faqs_page')
 
+    def form_valid(self, form):
+        response = super(FAQCreateView, self).form_valid(form)
+        messages.success(self.request, 'FAQ created successfully.')
+        return response
+
 class FAQUpdateView(LoginRequiredMixin, StaffRequiredMixin, UpdateView):
     model = FAQ
     form_class = FAQForm
     template_name = 'faqs/faq_form.html'
     success_url = reverse_lazy('faqs:faqs_page')
 
+    def form_valid(self, form):
+        response = super(FAQUpdateView, self).form_valid(form)
+        messages.success(self.request, 'FAQ updated successfully.')
+        return response
+
 class FAQDeleteView(LoginRequiredMixin, StaffRequiredMixin, DeleteView):
     model = FAQ
     template_name = 'faqs/faq_confirm_delete.html'
     success_url = reverse_lazy('faqs:faqs_page')
+
+    def delete(self, request, *args, **kwargs):
+        response = super(FAQDeleteView, self).delete(request, *args, **kwargs)
+        messages.success(self.request, 'FAQ deleted successfully.')
+        return response
