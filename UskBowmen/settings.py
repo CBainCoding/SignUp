30d1823c25d94133efe465a1357dce1c857f2caf
Charles Bainbridge <bainbridge.charles92@gmail.com>
2024-03-25 10:00:16 +0000

    Created new secret key, added to env.py and removed from settings.py

--- UskBowmen/settings.py
+++ UskBowmen/settings.py
@@ -26,7 +26,7 @@ TEMPLATES_DIR = BASE_DIR / 'templates'
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8mw0bmz1cv*%m3b@(#h)ex-fux9du731-=4%li1(qak12stxdd'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEBUG' in os.environ
